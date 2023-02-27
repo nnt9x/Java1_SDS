eda439ef93eeb78ebdef50d1d069553a2a33d4af
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-02-27 08:52:29 +0700

    Buổi 2b - sua bai tap

---
+++ src/main/java/day2bt/B111b.java
@@ -0,0 +1,36 @@
+package day2bt;
+
+import java.util.Scanner;
+
+public class B111b {
+    public static void main(String[] args) {
+        // Chiều cao h = 4, tối đa là 7 kí tự (2*h - 1)
+        Scanner in = new Scanner(System.in);
+        System.out.print("Nhap chieu cao h = ");
+        int h = in.nextInt();
+        in.close();
+        // Tinh so ki tu toi da
+        int m = 2 * h - 1;
+
+        // h = 4 -> lap 4 lan
+        for (int i = 1; i <= h; i++) {
+            // Can in ra chi tiet tung dong
+            // Tim ra vi tri in dau * -> comn lai thi in khoang trang
+            // Dong 1: vi tri [h-0; h+0]
+            // Dong 2:  [h-1, h+1]
+            // Dong 3: [h-2, h+2]
+            // Dong 4: [h-3; h+3]
+            for (int j = 1; j <= m; j++) {
+                int k = i - 1;
+                if (j == (h - k) || j == (h + k) || i == h) {
+                    System.out.print("*\t");
+                    continue;
+                }
+
+                System.out.print(" \t");
+            }
+            // Xuong dong trong moi vong lap
+            System.out.println();
+        }
+    }
+}
