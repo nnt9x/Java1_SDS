8d524b32da359489b80f0cbef00bebc99ccac73f
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-02-27 16:39:02 +0700

    Buoi 3: sua bt lap trinh huong doi tuong

---
+++ src/main/java/day3b/suabt/Main1.java
@@ -0,0 +1,12 @@
+package day3b.suabt;
+
+public class Main1 {
+    public static void main(String[] args) {
+        HinhChuNhat h1 = new HinhChuNhat();
+        h1.setChieuDai(3);
+        h1.setChieuRong(4);
+        h1.setChieuDai(-3);
+
+        System.out.println(h1);
+    }
+}
